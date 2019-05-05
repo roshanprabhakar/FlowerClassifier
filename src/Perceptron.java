021516d1e84ec0001c5977a63e4daa814ed5a89c
Roshan Prabhakar <roshan@aprabhakar.com>
2019-05-04 22:46:05 -0700

    implemented perceptron

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,19 +1,55 @@
 public class Perceptron {
 
-    private final double learningRate = 0.05f;
+    private String identity; //will classify for identity
+
+    private double learningRate = 0.05f;
+    private double threshhold = 0;
 
     private double[] weights;
-    private double[] features;
 
     //returns boolean value for whether or not a certain flower, eventually link to a bunch of perceptrons
-    public Perceptron(double[] input) {
-        this.features = input;
+    public Perceptron(String identity) {
+        this.identity = identity;
     }
 
     public void train(double[] input) {
 
+        //change the weights;
+        //change the threshhold;
+
+        //error = guess - actual, get direction of error
+        int error = guess(input) - getCorrectGuess(identity);
+
+        //adjust weights
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] += error * learningRate * input[i];
+        }
+
+        threshhold += error * learningRate;
     }
 
-    public int guess
+    //guess what feature vector could describe given current weights and threshhold
+    //this method is the classifying method
+    public int guess(double[] trainingPoint) {
+
+        double sum = 0;
+
+        for (int i = 0; i < trainingPoint.length; i++) {
+            sum += trainingPoint[i] * weights[i];
+        }
+
+        return activation(sum);
+
+    }
+
+    public int activation(double sum) {
+        if (sum > threshhold) return 1;
+        return 0;
+    }
+
+    public int getCorrectGuess(String label) {
+        if (label.equals(this.identity)) return 1;
+        return 0;
+    }
 
 }
