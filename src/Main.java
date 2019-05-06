1a707f6deaceee4f5544392500a60287a455e05d
Roshan Prabhakar <roshan@aprabhakar.com>
2019-05-05 23:01:38 -0700

    added printlns to track direction of weight change

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static String[] attributes = new String[]{"sepal_length", "sepal_width"};
     private static String parsingTarget = "Iris-setosa";
 
+    //end weights: 0.2550000037997961, 0.1750000026077032
     public static void main(String[] args) {
 
         DataLoader loader = new DataLoader("data.csv");
@@ -14,16 +15,18 @@ public class Main {
         ArrayList<FlowerData> trainingData = loader.getData();
         Perceptron perceptron = new Perceptron(parsingTarget, 2);
 
-
+        int line = 1;
         for (FlowerData data : trainingData) {
+//            System.out.println("line: " + line);
             perceptron.train(data);
+            line++;
         }
 
 //        perceptron.train(trainingData.get(0).getSpecifiedVector(attributes));
 
-        double[] testData = new double[]{5.4, 3.9}; //is setosa
-        double[] testData2 = new double[]{6.1, 2.8}; //is versicolor
+        FlowerData virginicaTest = new FlowerData("6.7,3.3,5.7,2.1,Iris-virginica"); //is virginica
+        FlowerData setosaTest = new FlowerData("4.8,3.4,1.9,0.2,Iris-setosa"); //is setosa
 
-        System.out.println(perceptron.guess(testData2));
+        System.out.println(perceptron.guess(virginicaTest));
     }
 }
--- src/Perceptron.java
+++ src/Perceptron.java
@@ -9,6 +9,8 @@ public class Perceptron {
 
     private double[] weights;
 
+    private String old = ""; //to change
+
     //returns boolean value for whether or not a certain flower, eventually link to a bunch of perceptrons
     public Perceptron(String identity, int vectorLength) {
         this.identity = identity;
@@ -23,20 +25,28 @@ public class Perceptron {
         //change the threshhold;
 
         //error = guess - actual, get direction of error
-        System.out.println("identity of input: " + identity);
+        System.out.println("identity of input: " + input.getIdentity());
         System.out.println("call to getCorrectGuess(above): " + getCorrectGuess(input.getIdentity()));
+
+        System.out.println("guess: " + guess(input));
 
         int error = guess(input) - getCorrectGuess(identity);
         System.out.println("error: " + error);
 
+        System.out.println("weights start as: " + Arrays.toString(weights));
         //adjust weights
         for (int i = 0; i < weights.length; i++) {
-            weights[i] += error * learningRate * input.getSpecifiedVector(Main.attributes)[i];
+            weights[i] -= error * learningRate * input.getSpecifiedVector(Main.attributes)[i];
             System.out.println("weights: " + Arrays.toString(weights));
         }
 
+        if (!input.getIdentity().equals(old)) {
+            old = input.getIdentity();
+            System.out.println(old);
+        }
+
         //adjust threshhold (vertical translation on 2D graph)
-        threshhold += error * learningRate;
+        threshhold -= error * learningRate;
         System.out.println("Threshhold: " + threshhold);
         System.out.println("-------------------");
     }
